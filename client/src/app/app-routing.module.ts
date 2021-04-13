b1a35250dd2f4a453da55a8f0a8f62b7888e8e7b
mehditounsi <mehditounsi1997@gmail.com>
2021-04-13 23:36:36 +0100

    home page & routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {BrandsComponent} from './components/brands/brands.component';
+import {AboutComponent} from './components/about/about.component';
+import {ContactComponent} from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component:HomePageComponent},
+{path:'Brands',component:BrandsComponent},
+{path:'About',component:AboutComponent},
+{path:'Contact',component:ContactComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    NavbarComponent
+    NavbarComponent,
+    BrandsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
