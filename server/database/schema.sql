768bbdf6982b95bb4cb1ab0d3e8ed2dc5acd6590
Rihem <rihambensaad@gmail.com>
2021-04-14 13:34:56 +0100

    update chef table

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -5,55 +5,58 @@ CREATE DATABASE darekitchen;
 USE darekitchen;
 
 CREATE TABLE chef (
-    ID INT AUTO_INCREMENT,
-    firstName VARCHAR(55) NOT NULL ,
-    lastName VARCHAR(55) NOT NULL ,
-    phoneNumber INT NOT NULL ,
-    location VARCHAR(255) NOT NULL ,
-    imageCardId VARCHAR(255) NOT NULL ,
-    PRIMARY KEY (ID)
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(55) NOT NULL,
+    lastName VARCHAR(55) NOT NULL,
+    phoneNumber INT NOT NULL,
+    locationChef VARCHAR(255) NOT NULL,
+    imageCardId VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE virtualBrands (
-    ID INT AUTO_INCREMENT,
-    name VARCHAR(55) NOT NULL ,
-    category VARCHAR(255) NOT NULL ,
-    menu VARCHAR(1000) NOT NULL ,
-    logo VARCHAR(255) NOT NULL ,
-    FOREIGN KEY (ID)
+CREATE TABLE brands (
+    ID_brands INT AUTO_INCREMENT PRIMARY KEY,
+    brandName VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    menu VARCHAR(255) NOT NULL,
+    logo VARCHAR(55) NOT NULL
 );
 
 CREATE TABLE menu (
-    ID INT AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL ,
-    description VARCHAR(255) NOT NULL ,
-    price INT NOT NULL , 
-    PRIMARY KEY (ID)       
+    ID_menu INT AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    descriptionMenu VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    ID_brands INT,
+    PRIMARY KEY (ID_menu),
+    FOREIGN KEY (ID_brands) REFERENCES brands(ID_brands)
 );
 
-CREATE TABLE user (
-    ID INT AUTO_INCREMENT,
-    firstName VARCHAR(55) NOT NULL ,
-    lastName VARCHAR(55) NOT NULL ,
-    email VARCHAR(55) NOT NULL ,
-    password VARCHAR(55) NOT NULL ,
-    phoneNumber INT NOT NULL ,
-    location VARCHAR(255) NOT NULL ,
-    FOREIGN KEY (ID)
-);
 
 CREATE TABLE admin (
     ID INT AUTO_INCREMENT,
-    userName VARCHAR(55) NOT NULL ,
-    email VARCHAR(55) NOT NULL ,
-    password VARCHAR(55) NOT NULL ,
+    userName VARCHAR(55) NOT NULL,
+    email VARCHAR(55) NOT NULL,
+    password VARCHAR(55) NOT NULL,
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE session (
-    ID INT AUTO_INCREMENT,
-    userId INT NOT NULL,
-    session  VARCHAR(255) NOT NULL,
-    date VARCHAR(55) NOT NULL,
-    PRIMARY KEY (ID)
+
+CREATE TABLE user (
+    ID_user INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(55) NOT NULL,
+    lastName VARCHAR(55) NOT NULL,
+    email VARCHAR(55) NOT NULL,
+    password VARCHAR(55) NOT NULL,
+    phoneNumber INT NOT NULL,
+    location VARCHAR(255) NOT NULL
+    );
+
+CREATE TABLE sessionn (
+    ID_session INT AUTO_INCREMENT,
+    sessionName  VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    PRIMARY KEY (ID_session),
+    ID_user INT,
+    PRIMARY KEY (ID_session),
+    FOREIGN KEY (ID_brands) REFERENCES brands(ID_brands)
 );
