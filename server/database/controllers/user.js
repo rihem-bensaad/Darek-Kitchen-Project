448fc867912b7a99175fdf8e627f0ab7f7804231
Rihem <rihambensaad@gmail.com>
2021-04-14 14:56:20 +0100

    get user by id

--- server/database/controllers/user.js
+++ server/database/controllers/user.js
@@ -10,3 +10,15 @@ module.exports.getuser = function (req, res) {
     }
   });
 };
+
+module.exports.getuserById = function (req, res) {
+  const sql = "SELECT * FROM user WHERE id = ?"
+  con.query(sql, [req.params.id],function (err, result) {
+    console.log(req.params);
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(result);
+    }
+  });
+};
