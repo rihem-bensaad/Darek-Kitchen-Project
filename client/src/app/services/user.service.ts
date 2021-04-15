b804ac3277a16df2b5cdba023f589cbea51bfa74
Rihem <rihambensaad@gmail.com>
2021-04-15 15:32:41 +0100

    merging

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -9,6 +9,9 @@ export class UserService {
   constructor(private http: HttpClient) {
   }
   postFrom(data:any) {
-     return this.http.post('http://localhost:8000/',data)
+     return this.http.post('http://localhost:8000/user/signup', data)
+  }
+  goEat(data:any) {
+    return this.http.get('http://localhost:8000/user/singin', data)
   }
 }
