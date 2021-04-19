799c415915be9ffda40dd207819bbc9f6f2a6b7b
Rihem <rihambensaad@gmail.com>
2021-04-19 16:52:49 +0100

    add component add brands

---
+++ client/src/app/components/add-brand/add-brand.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators , FormControl} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-brand',
+  templateUrl: './add-brand.component.html',
+  styleUrls: ['./add-brand.component.css']
+})
+export class AddBrandComponent implements OnInit {
+   brandForm = new FormGroup({
+    brandName: new FormControl(''),
+    category: new FormControl(''),
+    logo: new FormControl('')
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
