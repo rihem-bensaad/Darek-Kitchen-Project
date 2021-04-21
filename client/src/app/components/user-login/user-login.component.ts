765083c42a535052b030d42942d0ef0f2498591f
mehditounsi <mehditounsi1997@gmail.com>
2021-04-21 22:54:39 +0100

    commit before auth risk

--- client/src/app/components/user-login/user-login.component.ts
+++ client/src/app/components/user-login/user-login.component.ts
@@ -28,11 +28,32 @@ export class UserLoginComponent implements OnInit {
   postFrom() {
     this.authService.login(this.loginForm.value)
       .subscribe((result : any) => {
-       this.DecodeToken(result['token'])['role']
+      
         localStorage.setItem('data', JSON.stringify(result))
-        console.log(result)
-        this.router.navigate(['/'])
+        if(this.DecodeToken(result['token'])['role']==="user"){
+          this.router.navigate(['/userlogin'])
+
+        }else{
+          this.router.navigate(['/dashboard'])
+        }
+       
       })
   }
+  // submitted = false;
+  // constructor(private userService: UsersService ,private myform: FormBuilder, private router:Router) { }
+  // get f() { return this.userForm.controls; }
+  // signinUser(){ 
+  //   var value = this.userForm.value
+  //   if(value.email !== "admin" && value.password !=="admin"){
+  //     this.submitted = true;
+  //     this.userService.signinUser(this.userForm.value).subscribe((data) => {localStorage.setItem('user', Object.values(data)[1])
+  //     this.router.navigate(["/profil"])
+  //     })
+  //   }else{
+  //     this.submitted = true;
+  //     this.userService.signinUser(this.userForm.value).subscribe((data) => {localStorage.setItem('admin', Object.values(data)[1])})
+  //     setTimeout(() => {this.router.navigate(["/admin"])}, 1000)
+  //   }
+  // }
 
 }
