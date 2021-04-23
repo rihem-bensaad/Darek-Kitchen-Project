8d04c455129336684d5b2d02cc44412aa5ffb5cc
Rihem <rihambensaad@gmail.com>
2021-04-23 01:43:54 +0100

    post menu handler

---
+++ server/handlers/menuHandler.js
@@ -0,0 +1,5 @@
+module.exports.postmenu = (req,res)=>{
+db.postmenu([req.body.title, req.body.image , req.body.price, req.body.ID_brands],(err,result)=>{
+    err ? console.log(err) : res.status(201).send(result)
+})
+}
