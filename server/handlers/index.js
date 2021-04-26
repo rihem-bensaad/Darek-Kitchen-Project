6abf80d0a2efc50d6d7e9f48b6429e46d47a0c8f
mehditounsi <mehditounsi1997@gmail.com>
2021-04-26 12:31:00 +0100

    chef logs

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -4,3 +4,4 @@ module.exports.brand = require('./brandHandler');
 module.exports.admin = require('./adminHandler');
 module.exports.auth = require('./authHandler');
 module.exports.menu = require('./menuHandler');
+module.exports.chef = require('./chefHandler');
