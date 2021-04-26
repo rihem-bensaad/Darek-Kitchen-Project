1c22cba9775c2f26c0af4c48ba5bdc7508af9f1f
mehditounsi <mehditounsi1997@gmail.com>
2021-04-26 23:37:24 +0100

    commit before pull

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,7 +18,6 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import {IvyCarouselModule} from 'angular-responsive-carousel';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { AddBrandComponent } from './components/add-brand/add-brand.component';
-import { ListUserComponent } from './components/list-user/list-user.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { AddMenuComponent } from './components/add-menu/add-menu.component';
 import { GetUserComponent } from './components/get-user/get-user.component';
@@ -45,7 +44,6 @@ import { PlateComponent } from './components/plate/plate.component';
     AddBrandComponent,
     AdminDashboardComponent,
     AddBrandComponent,
-    ListUserComponent,
     MenuComponent,
     AddMenuComponent,
     GetUserComponent,
