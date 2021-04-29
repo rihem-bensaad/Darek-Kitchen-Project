df17facb16d32a151497a6a412bd1066d3bec96b
Rihem <rihambensaad@gmail.com>
2021-04-29 12:27:28 +0100

    subscribe the menu

--- client/src/app/components/cart/cart.component.ts
+++ client/src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../../services/menu.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -12,10 +13,14 @@ export class CartComponent implements OnInit {
   menu = this.menuService.getmenu();
   cartItem = []
 
-  constructor(private menuService: MenuService) { }
+  constructor(private menuService: MenuService , private msg: CartService) { }
 
   ngOnInit(): void {
     this.getmenu();
+
+    this.msg.getMsg().subscribe(menu => {
+      console.log(menu);
+    })
 
   }
     getmenu() {
