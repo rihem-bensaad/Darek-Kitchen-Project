d6a731d1dcf7312da01bf64de2f7f1a836c996b3
Rihem <rihambensaad@gmail.com>
2021-05-18 23:29:29 +0100

    before push my work

--- client/src/app/components/user-signup/user-signup.component.ts
+++ client/src/app/components/user-signup/user-signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators , FormControl} from '@angular/forms';
+import { FormGroup, Validators , FormControl} from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service'
 
@@ -9,7 +9,9 @@ import { UserService } from '../../services/user.service'
   styleUrls: ['./user-signup.component.css']
 })
 export class UserSignupComponent implements OnInit {
-   fieldTextType: boolean = false;
+
+  fieldTextType: boolean = false;
+
   signupForm = new FormGroup({
     firstName: new FormControl([''],[Validators.required]),
     lastName: new FormControl([''],[Validators.required]),
@@ -18,7 +20,6 @@ export class UserSignupComponent implements OnInit {
     phoneNumber: new FormControl([''],[Validators.required]),
     location: new FormControl([''],[Validators.required]),
   });
-
 
 
 
@@ -32,46 +33,34 @@ export class UserSignupComponent implements OnInit {
   }
 
 shouldShowFirstNameRequiredError() {
-
     const firstName = this.signupForm.controls.firstName;
-
     return firstName.touched && firstName.hasError('required');
+  }
 
-}
 shouldShowLastNameRequiredError() {
-
   const lastName = this.signupForm.controls.lastName;
-
   return lastName.touched && lastName.hasError('required');
+  }
 
-}
 shouldShowEmailRequiredError() {
   const email = this.signupForm.controls.email;
   return email.touched && email.hasError('required');
 }
 
 shouldShowPasswordRequiredError() {
-
   const password = this.signupForm.controls.password;
-
   return password.touched && password.hasError('required');
+  }
 
-}
 shouldShowPhoneNumberRequiredError() {
-
   const phoneNumber = this.signupForm.controls.phoneNumber;
-
   return phoneNumber.touched && phoneNumber.hasError('required');
+  }
 
-}
 shouldShowLocationRequiredError() {
-
   const location = this.signupForm.controls.location;
-
   return location.touched && location.hasError('required');
-
-}
-
+  }
 
   postFrom() {
     this.userService.postFrom(this.signupForm.value)
